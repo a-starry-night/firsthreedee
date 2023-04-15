4a93974e0e21dd73dc34b4fc5f7d4076736b9a75
wizard <wegwerfprofil@gmail.com>
2023-04-15 10:08:50 +0100

    init vite project

---
+++ main.js
@@ -0,0 +1,40 @@
+import './style.css';
+/*import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';*/
+import * as THREE from 'three';
+//import {}
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusGeometry(10, 2, 10, 30);
+const geometry1 = new THREE.TorusGeometry(10, 2, 10, 30);
+const mat = new THREE.MeshBasicMaterial({color:0xFF00FF, wireframe: true});
+const obj = new THREE.Mesh(geometry, mat);
+const obj1 = new THREE.Mesh(geometry1, mat);
+
+scene.add(obj);
+scene.add(obj1);
+
+function animate(){
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  obj.rotation.z += 0.01;
+  obj.rotation.y += 0.005;
+  obj.rotation.x += 0.0025;
+  obj1.rotation.z += 0.01;
+  obj1.rotation.y += 0.008;
+  obj1.rotation.x -= 0.0025;
+}
+
+animate();
