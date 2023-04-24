34269e1afb48c392e687c4163f82f6a4151ac77a
wizard <wegwerfprofil@gmail.com>
2023-04-24 13:29:08 +0100

    asdf

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import './style.css';
 /*import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.js';*/
-import * as THREE from 'three';
+import * as THREE from './node_modules/three';
 //import {}
 
 const scene = new THREE.Scene();
